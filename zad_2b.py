ccd83dde443df2ae7e1c4ea7828bded1488165d8
Ivetta <iweta.baczar@gmail.com>
2025-12-13 13:39:48 +0100

    add standard PEP8

--- zad_2b.py
+++ zad_2b.py
@@ -4,9 +4,12 @@ def multiply_list_for(numbers):
         result.append(number * 2)
     return result
 
+
 print(multiply_list_for([2, 3, 4, 2, 1]))
+
 
 def multiply_list(numbers):
     return [number * 2 for number in numbers]
+
 
 print(multiply_list([2, 3, 4, 2, 1]))
--- zad_2c.py
+++ zad_2c.py
@@ -3,5 +3,6 @@ def print_even_number(numbers):
         if number % 2 == 0:
             print(number)
 
+
 number_list = list(range(1, 11))
 print_even_number(number_list)
