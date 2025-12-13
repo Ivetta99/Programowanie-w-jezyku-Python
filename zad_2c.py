25fdaac166f1fd351cf2ea2d54383c6788d66e40
Ivetta <iweta.baczar@gmail.com>
2025-12-13 10:19:35 +0100

    Delete all zad_2 files

--- zad_2c.py
+++
@@ -1,7 +0,0 @@
-def print_even_number(numbers):
-    for number in numbers:
-        if number % 2 == 0:
-            print(number)
-
-number_list = list(range(1, 11))
-print_even_number(number_list)
